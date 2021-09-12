ab2150905426f3b5882d46eed322c22bf7308a86
Leul Yiheyis <leulykpro@gmail.com>
2021-09-12 17:31:16 +0300

    use the recursive algorithm

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,6 +3,33 @@
 #include "lists.h"
 
 /**
+ * is_palindrome_rec - check whether a singly linked list is a palindrome
+ *
+ * @left: pointer to the head node
+ * @right: the head node
+ *
+ * Return: 1 if it's a palindrome, otherwise 0 (an empty list is considered
+ * to be a palindrome)
+ *
+ */
+int is_palindrome_rec(listint_t **left, listint_t *right)
+{
+	int check1, check2;
+
+	if (right == NULL)
+		return (1);
+
+	check1 = is_palindrome_rec(left, right->next);
+	if (check1 == 0)
+		return (0);
+
+	check2 = ((*left)->n == right->n);
+	*left = (*left)->next;
+
+	return (check2);
+}
+
+/**
  * is_palindrome - check whether a singly linked list is a palindrome
  *
  * @head: pointer to the head node
@@ -13,36 +40,9 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
-	int i = 0, length = 0, *list;
+		listint_t *current = *head;
 
-	if (current == NULL)
-		return (1);
-	while (current)
-	{
-		current = current->next;
-		length++;
-	}
-	list = malloc(sizeof(int) * length);
-	if (list == NULL)
-		return (0);
-	current = *head;
-	i = 0;
-	while (current)
-	{
-		list[i] = current->n;
-		current = current->next;
-		i++;
-	}
-	for (i = 0; i <= (length - 1) / 2; ++i)
-	{
-		if (list[i] != list[length - 1 - i])
-		{
-			free(list);
-			return (0);
-		}
-	}
-	free(list);
-
-	return (1);
+		if (current == NULL)
+			return (1);
+		return (is_palindrome_rec(&current, current));
 }
