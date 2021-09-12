ccf9634fe4d308dc34945018596b22c5875e74f1
Leul Yiheyis <leulykpro@gmail.com>
2021-09-12 10:51:31 +0300

    [fixassign max to the first element

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,8 +4,8 @@
 def max_integer(my_list=[]):
     if my_list == []:
         return None
-    maxint = 0
-    for i in my_list:
-        if i > maxint:
-            maxint = i
+    maxint = my_list[0]
+    for i in range(1, len(my_list)):
+        if my_list[i] > maxint:
+            maxint = my_list[i]
     return maxint
