faf38d785f4f7f825f0ab42513f72d188e43f491
Leul Yiheyis <leulykpro@gmail.com>
2021-09-14 11:11:52 +0300

    remove unnecessary lines

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,8 +3,7 @@
 import sys
 
 if __name__ == "__main__":
-    length = len(sys.argv) - 1
     result = 0
-    for i in range(1, length + 1):
+    for i in range(1, len(sys.argv)):
         result += int(sys.argv[i])
     print("{}".format(result))
