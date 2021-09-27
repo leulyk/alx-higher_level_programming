435e50fae8e3ce0255ece264112ad0d5a0a9d0e3
Leul Yiheyis <leulykpro@gmail.com>
2021-09-27 15:00:34 +0300

    add additional exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except ValueError as ve:
+    except (ValueError, TypeError) as ve:
         print("Exception: {}".format(ve), file=sys.stderr)
         return False
     return True
