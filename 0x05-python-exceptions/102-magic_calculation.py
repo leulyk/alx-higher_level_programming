c2f24da139cbd65d2038b8fb27aa0435c8ddabcb
Leul Yiheyis <leulykpro@gmail.com>
2021-09-27 16:19:41 +0300

    add the try... except clause

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,9 +4,11 @@
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
-        if i > a:
-            raise Exception('Too far')
-        else:
+        try:
+            if i > a:
+                raise Exception('Too far')
             result += (a ** b) / i
-        result = b + a
+        except Exception:
+            result = b + a
+            break
     return result
