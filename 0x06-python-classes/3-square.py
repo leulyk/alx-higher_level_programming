59851085ce739a4f6613bbfb64cb7e1f2eaf0a33
Leul Yiheyis <leulykpro@gmail.com>
2021-09-28 18:36:19 +0300

    method to compute area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+class Square:
+    """ A Square class """
+    def __init__(self, size=0):
+        """ initialize square with a specific size
+        Args:
+            __size: size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ compute the area of a square
+        Return:
+            the area
+        """
+        return self.__size ** 2
