ffffa35da1553f38ec65ea7ddb776e2f48b2abd0
Leul Yiheyis <leulykpro@gmail.com>
2021-10-04 21:48:45 +0300

    add __repr__

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+
+"""
+    Project on python classes and objects
+    Rectangle class #3
+"""
+
+
+class Rectangle:
+    """ A class representing a rectangle """
+    def __init__(self, width=0, height=0):
+        """ constructor
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """ getter method for width private instance attribute """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ setter method for width private instance attribute """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """ getter method for height private instance attribute """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setter method for height private instance attribute """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        """ compute area of a rectangle """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ compute perimeter of a rectangle """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """ string representation of the rectangle class """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        output = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                output += "#"
+            if i != self.__height - 1:
+                output += "\n"
+        return output
