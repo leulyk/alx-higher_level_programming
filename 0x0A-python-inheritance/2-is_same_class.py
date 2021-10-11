2e4b2029ed655d58e1f156554cee434d09d784fb
Leul Yiheyis <leulykpro@gmail.com>
2021-10-11 22:26:19 +0300

    is same class?

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+    0x0A. Python - Inheritance, Task 2
+"""
+
+
+def is_same_class(obj, a_class):
+    """ check whether an object is an instance of a class """
+    return type(obj) == a_class
