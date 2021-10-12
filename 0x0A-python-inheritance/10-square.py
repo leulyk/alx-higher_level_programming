42a5698a9e212f621184b4fe90f5557e72461853
Leul Yiheyis <leulykpro@gmail.com>
2021-10-12 10:24:27 +0300

    Square class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+    0x0A. Python - Inheritance, Task 10
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ a class representing a square deriving from Rectangle """
+    def __init__(self, size):
+        """ constructor for the Square class """
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """ compute the area of a square """
+        return self.__size * self.__size
