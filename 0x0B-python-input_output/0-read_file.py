9549eec73738f11ba20ef7fb1452c6e209e08d4a
Leul Yiheyis <leulykpro@gmail.com>
2021-10-12 17:54:39 +0300

    read from file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" Python I/O Project, Task 0 """
+
+
+def read_file(filename=""):
+    """ read from a file """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read())
