e819b90ef829d130058c40d30769d4cf54cb7ce8
Leul Yiheyis <leulykpro@gmail.com>
2021-10-12 19:01:18 +0300

    class to json modified implementation

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,11 +4,11 @@
     Python I/O Project, Task 8
 """
 
-import json
-
 
 def class_to_json(obj):
     """ return the dictionary description with simple data
         structure for JSON serialization of an object
     """
-    return obj.__dict__
+    if hasattr(obj, '__dict__'):
+        return obj.__dict__.copy()
+    return {}
