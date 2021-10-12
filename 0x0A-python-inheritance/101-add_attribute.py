886b6c95f3cef38391ef289c7b87db952c5ba7cc
Leul Yiheyis <leulykpro@gmail.com>
2021-10-12 17:39:28 +0300

    add attribute

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+    0x0A. Python - Inheritance, Task 13
+"""
+
+
+def add_attribute(obj, attr, val):
+    """ adds an attribute to an object at runtime """
+    setattr(obj, attr, val)
