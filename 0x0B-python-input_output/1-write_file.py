75a43b86bece99a0bb6d4d2a13fba7c4862a5462
Leul Yiheyis <leulykpro@gmail.com>
2021-10-12 17:54:46 +0300

    write to file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" Python I/O Projects, Task 1 """
+
+
+def write_file(filename="", text=""):
+    """ write to a file """
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
