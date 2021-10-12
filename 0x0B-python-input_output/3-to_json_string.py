558c1a8170f608df779f0fe1b597954f045cb1aa
Leul Yiheyis <leulykpro@gmail.com>
2021-10-12 17:55:10 +0300

    to JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Python I/O Project, Task 3 """
+
+import json
+
+
+def to_json_string(my_obj):
+    """ return JSON representation of an object """
+    return json.dumps(my_obj)
