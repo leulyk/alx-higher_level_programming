a8548fc6374303f554afe32d017c41d2ae864240
Leul Yiheyis <leulykpro@gmail.com>
2021-10-12 21:03:20 +0300

    implementation of pascal's triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+""" Pascal's triangle """
+
+
+def pascal_triangle(n):
+    """ implement pascal triangle for a given integer
+    Args:
+        n: height of triangle
+    Returns: a list of lists of integers representing
+            pascal's triangle of n
+    """
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+    triangle = []
+    for i in range(n):
+        row = [0] * (i + 1)
+        triangle.append(row)
+    triangle[0][0] = 1
+    for row in range(n - 1):
+        for col in range(len(triangle[row])):
+            triangle[row + 1][col] += triangle[row][col]
+            triangle[row + 1][col + 1] += triangle[row][col]
+
+    return triangle
