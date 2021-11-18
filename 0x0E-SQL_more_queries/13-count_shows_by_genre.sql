821b537649816d71870c8bd12c6dcce5efbcd5d0
Leul Yiheyis <leulykpro@gmail.com>
2021-11-18 12:48:28 +0300

    make keywords uppercase

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- links all genres and the number of shows linked
-SELECT name as genre, COUNT(genre_id) as number_of_shows
+SELECT name AS genre, COUNT(genre_id) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON id = genre_id
