1bafb1aeabab135a4135055bb7994dc2240f0e60
Leul Yiheyis <leulykpro@gmail.com>
2021-11-18 13:01:06 +0300

    join all 3 tables

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,7 @@
 -- list the genres of the show 'Dexter'
 SELECT name FROM tv_genres
 INNER JOIN tv_show_genres
-ON id = genre_id
-WHERE show_id=8;
+ON tv_genres.id = genre_id
+INNER JOIN tv_shows
+ON tv_shows.id = show_id
+WHERE title="Dexter";
