406a7793e01c6d32d6bc8280b9dbb254b531d4b4
Leul Yiheyis <leulykpro@gmail.com>
2021-12-14 19:40:26 +0300

    add a print instance method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 1; i <= this.height; ++i) {
+      for (let j = 1; j <= this.width; ++j) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+}
+
+module.exports = Rectangle;
