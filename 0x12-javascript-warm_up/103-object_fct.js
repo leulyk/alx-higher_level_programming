2bec515dd8115bf6bbe75cbfb9c34df7732e000c
Leul Yiheyis <leulykpro@gmail.com>
2021-12-14 05:44:56 +0300

    increment object attribute value

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+/*
+Object.defineProperty(myObject, 'incr', {
+  value: function () {
+    myObject.value++;
+  }
+});
+*/
+
+myObject.incr = function incr () {
+  myObject.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
