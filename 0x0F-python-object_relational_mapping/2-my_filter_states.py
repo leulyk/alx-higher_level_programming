faafe6169173b77c2cc8ea174751b2c2d06f29e7
Leul Yiheyis <leulykpro@gmail.com>
2021-12-16 12:47:53 +0300

    fix pycodestyle E501 error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,6 +10,6 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (argv[4], ))
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (argv[4],))
     for row in cur.fetchall():
         print(row)
