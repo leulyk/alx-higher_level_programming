64266236ca492c280fd1640f9403832dd4c453b6
Leul Yiheyis <leulykpro@gmail.com>
2021-12-18 08:21:56 +0300

    Fix: use format() to create query with user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,6 +10,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (argv[4],))
+    cur.execute("SELECT * FROM states WHERE name='{}' \
+                 ORDER BY id".format(argv[4]))
     for row in cur.fetchall():
         print(row)
