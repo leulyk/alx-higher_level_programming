4b06ec392f86fdeaab90aadfd5d46633b3c8d366
Leul Yiheyis <leulykpro@gmail.com>
2021-12-18 08:32:59 +0300

    first model state

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+    First state model
+"""
+
+from sqlalchemy import String, Column, Integer
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+        class which is a blueprint for State instances
+        that links to  the MySQL table 'states'
+    """
+    __tablename__ = "states"
+    id = Column('id', Integer(), primary_key=True)
+    name = Column('name', String(128), nullable=False)
