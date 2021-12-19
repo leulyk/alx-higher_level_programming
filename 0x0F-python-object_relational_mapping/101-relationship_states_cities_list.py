db789b0377e4e55b8bb21855af35152aa752d03e
Leul Yiheyis <leulykpro@gmail.com>
2021-12-19 12:30:05 +0300

    list relationships

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+    A script that lists all State objects, and corresponding City objects:
+
+    Takes 3 arguments: mysql username, mysql password and database name
+    You must only use one query to the database
+    You must use the cities relationship for all State objects
+    Results must be sorted in ascending order by states.id and cities.id
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from relationship_state import Base, State
+from relationship_city import City
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+                            argv[1], argv[2], argv[3]))
+    session = Session(bind=engine)
+
+    states = session.query(State).all()
+
+    for index, state in enumerate(states):
+        print("{}: {}".format(index, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
