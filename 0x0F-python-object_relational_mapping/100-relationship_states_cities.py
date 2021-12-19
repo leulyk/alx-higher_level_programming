c9fe6d45718414a18659d01e25d2a3aaa5ec7e2e
Leul Yiheyis <leulykpro@gmail.com>
2021-12-19 12:15:57 +0300

    define relationship

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+    A script that creates the State “California” with the City “San Francisco”
+    from the database hbtn_0e_100_usa:
+
+    Takes 3 arguments: mysql username, mysql password and database name
+    You must use the cities relationship for all State objects
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from relationship_city import City
+from relationship_state import Base, State
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+                            argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    session = Session(bind=engine)
+
+    new_state = State()
+    new_state.name = "California"
+    new_city = City()
+    new_city.name = "San Francisco"
+    new_state.cities.append(new_city)
+
+    session.add(new_state)
+    session.add(new_city)
+
+    session.commit()
