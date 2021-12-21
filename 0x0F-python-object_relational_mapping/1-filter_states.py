7e32b06d6adf29bd863fdc266c7f16a1cecd3e0d
Leul Yiheyis <leulykpro@gmail.com>
2021-12-21 09:06:12 +0300

    query by binary comparison

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,6 +10,6 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE BINARY name like 'N%' ORDER BY id")
     for row in cur.fetchall():
         print(row)
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{}' \
+    cur.execute("SELECT * FROM states WHERE BINARY name='{}' \
                  ORDER BY id".format(argv[4]))
     for row in cur.fetchall():
         print(row)
