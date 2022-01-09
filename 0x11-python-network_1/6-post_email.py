3b31f699d0c2b17fe3be367106f12e7d22ebc311
Leul Yiheyis <leulykpro@gmail.com>
2022-01-09 19:16:47 +0300

    Use the data parameter not params

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,5 +12,5 @@ from sys import argv
 
 if __name__ == "__main__":
     values = {'email': argv[2]}
-    req = requests.post(argv[1], params=values)
+    req = requests.post(argv[1], data=values)
     print(req.text)
