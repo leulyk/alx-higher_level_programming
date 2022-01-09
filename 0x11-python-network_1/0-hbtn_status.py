dfc0e4dd5ec74efd900d4bec7f9045aa3c3e91e4
Leul Yiheyis <leulykpro@gmail.com>
2022-01-09 17:49:16 +0300

    Revert "try out by changing tab to spaces"
    This reverts commit 0f19ca43ec2989fb13d7a67e208c383b1d24264e.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         output = response.read()
         print('Body response:')
-        print('    - type: {}'.format(type(output)))
-        print('    - content: {}'.format(output))
-        print('    - utf8 content: {}'.format(output.decode('utf-8')))
+        print('\t- type: {}'.format(type(output)))
+        print('\t- content: {}'.format(output))
+        print('\t- utf8 content: {}'.format(output.decode('utf-8')))
