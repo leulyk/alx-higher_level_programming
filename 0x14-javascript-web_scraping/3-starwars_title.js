adcd9c70ee12ef521caaf367496f17814b5b1f6b
Leul Yiheyis <leulykpro@gmail.com>
2022-02-02 08:27:58 +0300

    print the title of the Star Wars movie fetched from an API

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, (error, response, body) => {
+  if (!error) {
+    const info = JSON.parse(body);
+    console.log(info.title);
+  }
+});
